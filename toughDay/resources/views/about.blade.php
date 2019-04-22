766ee021aa025a76b7b6d3f340e9d065f71b227a
Kowrog77 <43824900+Kowrog77@users.noreply.github.com>
2019-04-22 08:06:29 -0500

    toughDay

---
+++ toughDay/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends("layout")
+@section("heading")
+<h1>About Page</h1>
+@endsection 
+@section("body")
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas eaque iure corrupti sapiente reprehenderit doloribus illo similique dolorum culpa. Et aliquam asperiores saepe reprehenderit non! Illo, distinctio placeat. Nisi, voluptates.</p>
+<h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat natus eos explicabo praesentium dignissimos facere? Natus voluptas nostrum velit ducimus impedit pariatur sit beatae, qui maxime deserunt officia facere. Delectus!</h2>
+@endsection
+@section("title","My About page")
---
+++ toughDay/resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends("layout")
+@section("heading")
+<h1>Contact Page</h1>
+@endsection 
+@section("body")
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas eaque iure corrupti sapiente reprehenderit doloribus illo similique dolorum culpa. Et aliquam asperiores saepe reprehenderit non! Illo, distinctio placeat. Nisi, voluptates.</p>
+@endsection
+@section("title","My Contact  page")
